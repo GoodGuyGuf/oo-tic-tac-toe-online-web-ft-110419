9662aa7ef7504cbe4ed4103b8d5ee419d7078c64
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-20 22:52:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,10 @@ WIN_COMBINATIONS = [
 #that returns true if the board is full and has not been won,
 #false if the board is won, and false if the board is neither won nor full.
   def draw?
-  
+  if full?
+    false
+  elsif won?
+    false
   end
 
   def over?
