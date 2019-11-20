42f97a4c9f4550c2240b0d16b8c3f36e229e033f
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-20 22:02:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,12 +94,7 @@ WIN_COMBINATIONS = [
     #full should take every element as either an x or an o.
 
     #if the board length is less than the board count it should return false
-    if @board.length == @board.length
-      true
-    elsif @board.length < @board.length
-      false
-    end
-  end
+    
 
 
   def draw?
