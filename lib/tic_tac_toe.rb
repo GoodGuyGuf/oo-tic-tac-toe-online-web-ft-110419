1213165f9c45ddc1d97b2ce3677ca085b50c388b
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-20 18:57:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,10 +52,10 @@ WIN_COMBINATIONS = [
   turn_count % 2 == 0 ? "X" : "O"
   end
 
-  def turn(input)
+  def turn
     puts "Play with numbers 1-9!"
     gets current_player
-    input_to_index(input)
+
   end
 
 
