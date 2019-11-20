fa87b97a3f82ae0a69bb9a65763edb1757077e02
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-20 22:22:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,11 +86,11 @@ WIN_COMBINATIONS = [
   #Should return true for a draw
   #should return false if the board isn't filled
   #Should return true if every element in @board contains either an X or O
+  # if the board length is less than 9 then it is not full.
+  # if the board length has a winner or a draw then it is full.
   def full?
- # if the board length is less than 9 then it is not full.
- # if the board length has a winner or a draw then it is full.
-  
-end
+
+  end
 
 
   def draw?
