e49b46ca096eb1b120289fcbb11d9c6bf7296a06
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-20 22:06:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,7 +88,10 @@ WIN_COMBINATIONS = [
   #should return false if the board isn't filled
   #Should return true if every element in @board contains either an X or O
   def full?
-    
+    if @board.length == @board.length && won? == false
+      true
+    end
+  end
 
 
 
