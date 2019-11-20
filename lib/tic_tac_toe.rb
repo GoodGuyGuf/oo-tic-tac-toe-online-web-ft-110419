02d824a17b8e82a173b1eaadaaa9b2f21b04c483
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-20 20:13:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,7 @@ WIN_COMBINATIONS = [
     puts "Play using numbers 1-9!"
     input = gets
     index = input_to_index(input)
-
+    if valid_move?(index)
     move(index, current_player)
     display_board
     
